a9cbae11e5d58c93e22ded3ef51336897e7706cb
OssamaSabihi <sabihiossama318@gmail.com>
2023-06-23 03:53:29 -0700

    y

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * print_numbers -  prints the numbers, from 0 to 9
+ * followed by a new line
+ *
+ * Rturn: always 0
+*/
+
+void print_numbers(void)
+{
+	int a = 0;
+
+	for (a < 9; a++)
+		_putchar(a);
+
+	return (0);
+}
