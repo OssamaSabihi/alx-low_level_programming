fbc221fa32fd9a6fe530446e7d8b62cae1c685ef
OssamaSabihi <sabihiossama318@gmail.com>
2023-06-23 10:30:22 -0700

    le

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,9 @@ void more_numbers(void)
 
 	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (j > 11)
+			if (j >= 10)
 				_putchar('1');
 			_putchar (j % 10 + '0');
 		}
