1edd419ef7ce0fa07ad750ba3f370b1969fc1d76
OssamaSabihi <sabihiossama318@gmail.com>
2023-07-18 14:22:37 -0700

    dogo

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+* struct dog - struct dog
+* @name: name s dog
+* @age: age s dog
+* @owner:  owner s the dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
